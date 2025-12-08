0dca73cf09e9b2f3cba033c79bedf0af31ce4ebe
yamuna <yamunajayan@gmail.com>
2025-12-08 12:45:04 -0500

    add storiesData.js file

---
+++ src/data/storiesData.js
@@ -0,0 +1,226 @@
+import mountainMobile from "../assets/images/stories/mobile/mountains.jpg";
+import mountainDesktop from "../assets/images/stories/desktop/mountains.jpg";
+
+import cityScapesMobile from "../assets/images/stories/mobile/cityscapes.jpg";
+import cityScapesDesktop from "../assets/images/stories/desktop/cityscapes.jpg";
+
+import DaysVoyageMobile from "../assets/images/stories/mobile/18-days-voyage.jpg";
+import DaysVoyageDesktop from "../assets/images/stories/desktop/18-days-voyage.jpg";
+
+import architecturalsMobile from "../assets/images/stories/mobile/architecturals.jpg";
+import architecturalsDesktop from "../assets/images/stories/desktop/architecturals.jpg";
+
+import worldTourMobile from "../assets/images/stories/mobile/world-tour.jpg";
+import worldTourDesktop from "../assets/images/stories/desktop/world-tour.jpg";
+
+import unforseenCornersMobile from "../assets/images/stories/mobile/unforeseen-corners.jpg";
+import unforseenCornersDesktop from "../assets/images/stories/desktop/unforeseen-corners.jpg";
+
+import kingOnAfricaMobile from "../assets/images/stories/mobile/king-on-africa.jpg";
+import kingOnAfricaDesktop from "../assets/images/stories/desktop/king-on-africa.jpg";
+
+import tripToNowhereMobile from "../assets/images/stories/mobile/trip-to-nowhere.jpg";
+import tripToNowhereDesktop from "../assets/images/stories/desktop/trip-to-nowhere.jpg";
+
+import rageOfTheSeaMobile from "../assets/images/stories/mobile/rage-of-the-sea.jpg";
+import rageOfTheSeaDesktop from "../assets/images/stories/desktop/rage-of-the-sea.jpg";
+
+import runningFreeMobile from "../assets/images/stories/mobile/running-free.jpg";
+import runningFreeDesktop from "../assets/images/stories/desktop/running-free.jpg";
+
+import behindTheWavesMobile from "../assets/images/stories/mobile/behind-the-waves.jpg";
+import behindTheWavesDesktop from "../assets/images/stories/desktop/behind-the-waves.jpg";
+
+import calmWaterMobile from "../assets/images/stories/mobile/calm-water.jpg";
+import calmWaterDesktop from "../assets/images/stories/desktop/calm-water.jpg";
+
+import milkyWayMobile from "../assets/images/stories/mobile/milky-way.jpg";
+import milkyWayDesktop from "../assets/images/stories/desktop/milky-way.jpg";
+
+import darkForestMobile from "../assets/images/stories/mobile/dark-forest.jpg";
+import darkForestDesktop from "../assets/images/stories/desktop/dark-forest.jpg";
+
+import somwarpetMobile from "../assets/images/stories/mobile/somwarpet.jpg";
+import somwarpetDesktop from "../assets/images/stories/desktop/somwarpet.jpg";
+
+import landOfDreamsMobile from "../assets/images/stories/mobile/land-of-dreams.jpg";
+import landOfDreamsDesktop from "../assets/images/stories/desktop/land-of-dreams.jpg";
+
+export const stories = [
+  {
+    id: 1,
+    image: {
+      mobile: mountainMobile,
+      tablet: mountainDesktop,
+      desktop: mountainDesktop,
+    },
+    title: "The Mountains",
+    photographer: "John Appleseed",
+    date: "April 16th 2020",
+  },
+  {
+    id: 2,
+    image: {
+      mobile: cityScapesMobile,
+      tablet: cityScapesDesktop,
+      desktop: cityScapesDesktop,
+    },
+    title: "Sunset Cityscapes",
+    photographer: "Benjamin Cruz",
+    date: "April 14th 2020",
+  },
+  {
+    id: 3,
+    image: {
+      mobile: DaysVoyageMobile,
+      tablet: DaysVoyageDesktop,
+      desktop: DaysVoyageDesktop,
+    },
+    title: "18 Days Voyage",
+    photographer: "Alexei Borodin",
+    date: "April 11th 2020",
+  },
+  {
+    id: 4,
+    image: {
+      mobile: architecturalsMobile,
+      tablet: architecturalsDesktop,
+      desktop: architecturalsDesktop,
+    },
+    title: "Architecturals",
+    photographer: "Samantha Brooke",
+    date: "April 9th 2020",
+  },
+  {
+    id: 5,
+    image: {
+      mobile: worldTourMobile,
+      tablet: worldTourDesktop,
+      desktop: worldTourDesktop,
+    },
+    title: "World Tour 2019",
+    photographer: "Timothy Wagner",
+    date: "April 7th 2020",
+  },
+  {
+    id: 6,
+    image: {
+      mobile: unforseenCornersMobile,
+      tablet: unforseenCornersDesktop,
+      desktop: unforseenCornersDesktop,
+    },
+    title: "Unforeseen Corners",
+    photographer: "William Malcolm",
+    date: "April 2nd 2020",
+  },
+  {
+    id: 7,
+    image: {
+      mobile: kingOnAfricaMobile,
+      tablet: kingOnAfricaDesktop,
+      desktop: kingOnAfricaDesktop,
+    },
+    title: "King On Africa",
+    photographer: "Timothy Wagner",
+    date: "March 29th 2020",
+  },
+  {
+    id: 8,
+    image: {
+      mobile: tripToNowhereMobile,
+      tablet: tripToNowhereDesktop,
+      desktop: tripToNowhereDesktop,
+    },
+    title: "Trip To Nowhere",
+    photographer: "William Malcolm",
+    date: "March 21st 2020",
+  },
+  {
+    id: 9,
+    image: {
+      mobile: rageOfTheSeaMobile,
+      tablet: rageOfTheSeaDesktop,
+      desktop: rageOfTheSeaDesktop,
+    },
+    title: "Rage Of The Sea",
+    photographer: "Samantha Brooke",
+    date: "March 19th 2020",
+  },
+  {
+    id: 10,
+    image: {
+      mobile: runningFreeMobile,
+      tablet: runningFreeDesktop,
+      desktop: runningFreeDesktop,
+    },
+    title: "Running Free",
+    photographer: "Michelle",
+    date: "March 16th 2020",
+  },
+  {
+    id: 11,
+    image: {
+      mobile: behindTheWavesMobile,
+      tablet: behindTheWavesDesktop,
+      desktop: behindTheWavesDesktop,
+    },
+    title: "Behind The Waves",
+    photographer: "Michelle",
+    date: "March 9th 2020",
+  },
+  {
+    id: 12,
+    image: {
+      mobile: calmWaterMobile,
+      tablet: calmWaterDesktop,
+      desktop: calmWaterDesktop,
+    },
+    title: "Calm Water",
+    photographer: "Samantha Brooke",
+    date: "March 9th 2020",
+  },
+  {
+    id: 13,
+    image: {
+      mobile: milkyWayMobile,
+      tablet: milkyWayDesktop,
+      desktop: milkyWayDesktop,
+    },
+    title: "Milky Way",
+    photographer: "Benjamin Cruz",
+    date: "March 5th 2020",
+  },
+  {
+    id: 14,
+    image: {
+      mobile: darkForestMobile,
+      tablet: darkForestDesktop,
+      desktop: darkForestDesktop,
+    },
+    title: "Night at The Dark Forest",
+    photographer: "Mohammad Abdul",
+    date: "March 4th 2020",
+  },
+  {
+    id: 15,
+    image: {
+      mobile: somwarpetMobile,
+      tablet: somwarpetDesktop,
+      desktop: somwarpetDesktop,
+    },
+    title: "Somwarpet",
+    photographer: "Michelle",
+    date: "March 1st 2020",
+  },
+  {
+    id: 16,
+    image: {
+      mobile: landOfDreamsMobile,
+      tablet: landOfDreamsDesktop,
+      desktop: landOfDreamsDesktop,
+    },
+    title: "Land Of Dreams",
+    photographer: "William Malcolm",
+    date: "February 25th 2020",
+  },
+];
