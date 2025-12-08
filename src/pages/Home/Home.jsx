8295e3718a51da13cc95d42adc45c7c48f361183
yamuna <yamunajayan@gmail.com>
2025-12-07 22:43:10 -0500

    add homepage details section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../../components/Header/Header";
 import Hero from "../../components/Hero/Hero";
+import HomeDetailsSection from "../../components/HomeDetailsSection/HomeDetailsSection";
 
 const Home = () => {
   return (
     <div>
       <Header />
       <Hero />
+      <HomeDetailsSection />
     </div>
   );
 };
